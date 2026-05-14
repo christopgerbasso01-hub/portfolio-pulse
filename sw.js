0965c8b983163fff9a5319b08a52d841495b8450
christopgerbasso01-hub <christopger.basso01@gmail.com>
2026-05-14 14:55:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portfolio-pulse-v1';
+const CACHE = 'portfolio-pulse-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
